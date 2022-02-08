58faf1f9914c4385b1397b312c9c10fa40c28a7d
2giduck <cosmos7256@naver.com>
2022-02-08 23:39:27 +0900

    ENH: init commit

---
+++ src/test/java/topia/duck/market/MarketProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package topia.duck.market;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
